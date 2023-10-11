b1e3f587c061c236a8ffdfa2ec65b83335e64a52
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2023-10-11 16:45:22 +0700

    feat: make `Icon` conforms to `Codable`

--- Sources/AppIcon/Icon.swift
+++ Sources/AppIcon/Icon.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-public struct Icon: Identifiable {
+public struct Icon: Codable {
     public let key: String
     public let label: String
     public let iconName: String
     public let isDefault: Bool
-    
-    public var id: String {
-        key
-    }
     
     public init(key: String, label: String, iconName: String, isDefault: Bool) {
         self.key = key
         self.label = label
         self.iconName = iconName
         self.isDefault = isDefault
+    }
+}
+
+extension Icon: Identifiable {
+    public var id: String {
+        key
     }
 }
