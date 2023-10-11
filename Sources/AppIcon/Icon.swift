fffb381301c3a898b0acd11cf3e4fb06fc6cd8e9
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2023-10-11 09:27:20 +0700

    feat: make `Icon` conforms to `Identifiable`

--- Sources/AppIcon/Icon.swift
+++ Sources/AppIcon/Icon.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 
-public struct Icon {
+public struct Icon: Identifiable {
     public let key: String
     public let label: String
     public let iconName: String
     public let isDefault: Bool
+    
+    public var id: String {
+        key
+    }
     
     public init(key: String, label: String, iconName: String, isDefault: Bool) {
         self.key = key
